d66d1abfd60444dcb8a2a13d1ff83491f14f025d
Myranda Swartzwelter <myranda.swartzwelter@guildeducation.com>
2022-02-13 21:53:43 -0700

    add a little more

--- Assignments/assignment_week8_9/housing_MSwartzwelter.R
+++ Assignments/assignment_week8_9/housing_MSwartzwelter.R
@@ -12,11 +12,13 @@ housing_df <- read.csv("assignment_week8_9/week-6-housing.csv")
 #Import useful libraries
 #install.packages('dplyr')
 #install.packages('purrr')
-#install.packages('stringr')
+install.packages('car')
 
 library('dplyr')
 library('purrr')
 library('stringr')
+library('broom')
+library('car')
 
 #Transforamtions / modifications:
 
@@ -56,3 +58,65 @@ simple_lm$coef
 
 #get the standardized betas for the more complex model
 multi_lm$coef
+
+#A standardized beta coefficient  compares the strength of the effect of each individualind variable to the 
+#dependent variable. The higher the coefficient, the stronger the effect.
+
+#So, in my model I would expect the number of bedrooms and bathrooms to be the 
+#strongest indicator for sale price.
+
+#Calculate confidence intervals
+
+confint(simple_lm, "sq_ft_lot")
+
+confint(multi_lm, "sq_ft_lot")
+confint(multi_lm, "bedrooms")
+confint(multi_lm, "bath_full_count")
+confint(multi_lm, "year_built")
+confint(multi_lm,"year_renovated")
+
+#compare models using ANOVA() 
+anova(simple_lm, multi_lm)
+
+#Since we see a very small P Value of <2.2e-16 we can conclude that the more complex model has a better fit
+
+# Assessing Outliers
+#Look at leverage plots and outlier tests for each model
+outlierTest(simple_lm) # Bonferonni p-value for most extreme obs
+qqPlot(simple_lm, main="QQ Plot") #qq plot for resid
+leveragePlots(simple_lm) # leverage plots to view outiers
+
+simple_lm.diag.metrics <- augment(simple_lm)
+head(simple_lm.diag.metrics)
+
+outlierTest(multi_lm) # Bonferonni p-value for most extreme obs
+qqPlot(multi_lm, main="QQ Plot") #qq plot for studentized resid
+leveragePlots(multi_lm) # leverage plots to view outliers
+
+multi_lm.diag.metrics <- augment(multi_lm)
+head(multi_lm.diag.metrics)
+
+#calculate residuals
+simple_resid <- resid(simple_lm)
+multi_resid <- resid(multi_lm)
+
+#calculate standard residuals
+rstandard(simple_lm)
+rstandard(multi_lm)
+
+large_resids_simple <- rstandard(simple_lm)[abs(rstandard(simple_lm)) > 2]
+
+large_resids_multi <- rstandard(multi_lm)[abs(rstandard(multi_lm)) > 2]
+
+#Simple regression cooks distance
+cd_simple_add = cooks.distance(simple_lm)
+sum(cd_simple_add > 4 / length(cd_simple_add))
+large_cd_simple = cd_simple_add > 4 / length(cd_simple_add)
+cd_simple_add[large_cd_simple]
+
+#multiple parameters cooks distance
+cd_multi_add = cooks.distance(multi_lm)
+sum(cd_multi_add > 4 / length(cd_multi_add))
+large_cd_multi = cd_multi_add > 4 / length(cd_multi_add)
+cd_simple_add[large_cd_multi]
+
